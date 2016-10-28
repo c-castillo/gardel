195bc1b859c342b494b8092e69f727810d265316
vagrant <vagrant@kleer-csd-box>
2016-10-28 14:54:42 +0200

    Add ingresar_letra method, tests and refactor of initialize

--- lib/ahorcado.rb
+++ lib/ahorcado.rb
@@ -1,9 +1,22 @@
 class Ahorcado
-	attr_reader :palabra
-	 @@PALABRAS = ['AEROPUERTO', 'RAVIOLES', 'MILANESA', 'ARROZ']
+  attr_reader :palabra
+   @@PALABRAS = ['AEROPUERTO', 'RAVIOLES', 'MILANESA', 'ARROZ']
 
-	def initialize
-		@palabra = @@PALABRAS[Random.rand(@@PALABRAS.size-1)]
-	end
+  def initialize
+    @palabra = @@PALABRAS[Random.rand(@@PALABRAS.size-1)]
+    @letra = ''
+  end
 
+  def ingresar_letra(letra)
+    @letra = letra
+    validar_ingreso
+  end
+
+
+  def validar_ingreso
+    if @letra.length == 1
+      return true
+    end
+    return false
+  end
 end
--- spec/ahorcado_spec.rb
+++ spec/ahorcado_spec.rb
@@ -1,8 +1,16 @@
 require './lib/ahorcado'
 
-describe 'Juego Ahorcado' do
-	it "genera palabra" do
-		juego = Ahorcado.new
-		expect(juego.palabra.to_s.size).to be > 1 
-	end
+  describe 'Juego Ahorcado' do
+
+    it "genera palabra" do
+      juego = Ahorcado.new
+      expect(juego.palabra.to_s.size).to be > 1 
+    end
+
+    it 'validar el ingreso de una sola letra' do
+      a = Ahorcado.new
+      a.ingresar_letra('A')
+      expect(a.validar_ingreso).to eq true
+    end
+
 end
